d6511e5724fc20bc5bcb09d0380366ae4ac8aea0
Down709 <adhamel3ashry@gmail.com>
2025-10-18 23:10:57 +0300

    Create EmployeeUser.java

---
+++ src/EmployeeUser.java
@@ -0,0 +1,23 @@
+public class EmployeeUser {
+    private String employeeId;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+
+    public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String lineRepresentation() {
+        return employeeId + "," + name + "," + email + "," + address + "," + phoneNumber;
+    }
+
+    public String getSearchKey() {
+        return employeeId;
+    }
+}
